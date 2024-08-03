22edc586db449dd3be8d9e0649e518c7e4068b1b
ervinjahic65 <ervinjahic65@gmail.com>
2024-08-03 17:18:40 +0200

    add all items in header component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
 
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    [RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules
+    })],
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
