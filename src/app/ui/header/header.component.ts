aeb62fec7a880bbdc7088e95f2089efe226af6d9
ervinjahic65 <ervinjahic65@gmail.com>
2024-08-04 00:31:33 +0200

    add modal on search icon

--- src/app/ui/header/header.component.ts
+++ src/app/ui/header/header.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  isModalOpen = false;
+
+  openSearchModal() {
+    this.isModalOpen = true;
+  }
+
+  closeSearchModal() {
+    this.isModalOpen = false;
+  }
 
 }
