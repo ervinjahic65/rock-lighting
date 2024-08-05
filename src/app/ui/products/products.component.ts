7e1d4b37dc7a0f0410f693cc1d7120d3773f4416
ervinjahic65 <ervinjahic65@gmail.com>
2024-08-05 20:51:11 +0200

    add popular products section

--- src/app/ui/products/products.component.ts
+++ src/app/ui/products/products.component.ts
@@ -28,4 +28,31 @@ export class ProductsComponent {
       buttonText: 'SHOP NOW'
     }
   ];
+
+  popularProducts = [
+    {
+      image: '../../../assets/images/Group 494.png',
+      heading: 'ELITE',
+      subHeading: 'SCH6-LED-SM',
+      paragraph: '6 Inch Architectural High Lumen LED Cylinder'
+    },
+    {
+      image: '../../../assets/images/Group 493.png',
+      heading: 'DMF LIGHTING',
+      subHeading: 'DMF DRDHNJD',
+      paragraph: 'SquareFrame LED Housing'
+    },
+    {
+      image: '../../../assets/images/Group 492.png',
+      heading: 'ELITE',
+      subHeading: 'Elite FPR4-LED',
+      paragraph: 'LED round pendant cylinder'
+    },
+    {
+      image: '../../../assets/images/Group 491.png',
+      heading: 'WESTGATE',
+      subHeading: 'SCX-2FT-20W-30K-D',
+      paragraph: 'LED 2-3/4” Superior Architectural Seamless Linear Lights'
+    }
+  ];
 }
